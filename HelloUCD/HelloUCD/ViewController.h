a5ae24c8d947d2d12fbff97cf6008e458482698d
Ruaidhri Hallinan <RuaidhriHallinan@gmail.com>
2013-01-14 17:13:53 +0000

    First UCD Hello World commit

---
+++ HelloUCD/HelloUCD/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloUCD
+//
+//  Created by Ruaidhri Hallinan on 14/01/2013.
+//  Copyright (c) 2013 Ruaidhri Hallinan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
