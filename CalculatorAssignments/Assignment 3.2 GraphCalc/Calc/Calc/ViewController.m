8155775701dc3d0ce717f16d9c966434ac817485
Ruaidhri Hallinan <RuaidhriHallinan@gmail.com>
2013-02-04 21:19:08 +0000

    Updating folder names

---
+++ CalculatorAssignments/Assignment 3.2 GraphCalc/Calc/Calc/ViewController.m
@@ -0,0 +1,83 @@
+//
+//  ViewController.m
+//  Calc
+//
+//  Created by Ruaidhri Hallinan on 30/01/2013.
+//  Copyright (c) 2013 Ruaidhri Hallinan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+@synthesize calcModel = _calcModel;
+@synthesize calcDisplay = _calcDisplay;
+@synthesize isInTheMiddleOfTypingSomething = _isInTheMiddleOfTypingSomething;
+
+- (IBAction)digitPressed:(UIButton *)sender {
+    NSLog(@"digitPressed!");
+    NSString *digit = sender.titleLabel.text;
+    
+    //This might need a tidy up
+    if([digit isEqual:@"0"] && [self.calcModel.waitingOperation isEqual:@"/"]){
+            NSLog(@"digit: %@ ", digit);
+            NSLog(@"operand: %@ ", self.calcModel.waitingOperation);
+            // validation of dividing by 1 UIAlertView example
+            
+            UIAlertView *alert = [[UIAlertView alloc]
+                                  initWithTitle: @"Invalid"
+                                  message: @"You cant divide by 0!"
+                                  delegate: nil
+                                  cancelButtonTitle:@"OK"
+                                  otherButtonTitles:nil];
+            [alert show];
+        
+        // UILabel example
+        //self.errorDisplay.text = @"Cant divide by 0!";
+    }
+
+    if(self.isInTheMiddleOfTypingSomething)
+        self.calcDisplay.text =
+        [self.calcDisplay.text stringByAppendingString:digit];
+    else {
+        [self.calcDisplay setText:digit];
+        self.isInTheMiddleOfTypingSomething = YES;
+    }
+}
+
+- (IBAction)operationPressed:(UIButton *)sender {
+    NSLog(@"operationPressed!");
+    if(self.isInTheMiddleOfTypingSomething) {
+        self.calcModel.operand = [self.calcDisplay.text doubleValue];
+        self.isInTheMiddleOfTypingSomething = NO;
+    }
+    
+    NSString *operation = [[sender titleLabel] text];
+    double result = [[self calcModel] performOperation:operation];
+    [self.calcDisplay setText:[NSString stringWithFormat:@"%g", result]];
+}
+
+// noticing a bug here when adding two floating point numbers, it wont allow a decimal in the second number
+- (IBAction)pointPressed:(UIButton *)sender {
+    NSLog(@"pointPressed!");
+    NSRange range = [self.calcDisplay.text rangeOfString:@"."];
+    if(range.location == NSNotFound) {
+            self.calcDisplay.text = [self.calcDisplay.text stringByAppendingString:@"."];
+    }
+    self.isInTheMiddleOfTypingSomething = YES;
+}
+
+- (IBAction)backspacePressed:(UIButton *)sender {
+    
+    if ( [self.calcDisplay.text isEqualToString:@""]
+        || [self.calcDisplay.text isEqualToString:@"0"]) {
+        
+        self.calcDisplay.text = @"0";
+        self.isInTheMiddleOfTypingSomething = NO;
+    }
+    else {
+        self.calcDisplay.text =[self.calcDisplay.text substringToIndex: [self.calcDisplay.text length] - 1];
+        self.isInTheMiddleOfTypingSomething = YES;
+    }
+}
+@end
---
+++ CalculatorAssignments/Assignment 3.2 GraphCalc/Calc/CalcModel.m
@@ -0,0 +1,111 @@
+//
+//  CalcModel.m
+//  Calc
+//
+//  Created by Ruaidhri Hallinan on 30/01/2013.
+//  Copyright (c) 2013 Ruaidhri Hallinan. All rights reserved.
+//
+
+#import "CalcModel.h"
+@implementation CalcModel
+
+@synthesize waitingOperand = _waitingOperand;
+@synthesize waitingOperation = _waitingOperation;
+@synthesize operand = _operand;
+@synthesize memory = _memory;
+
+- (double) performOperation:(NSString *)operation {
+    
+    if([operation isEqual:@"sqrt"]) {
+        NSLog(@"sqrt : performingOperation!");
+        self.operand = sqrt(self.operand);
+    }
+    else if ([@"+/-" isEqualToString:operation]) {
+        NSLog(@"+/- : performingOperation!");
+        self.operand = -self.operand;
+    }
+    else if ([@"sin" isEqualToString:operation]) {
+        NSLog(@"sin : performingOperation!");
+        self.operand = sin(self.operand);
+    }
+    else if ([@"cos" isEqualToString:operation]) {
+        NSLog(@"cos : performingOperation!");
+        self.operand = cos(self.operand);
+    }
+    // including tangent button, even though its not asked for
+    // sin and cos dont look right without tan :p
+    else if ([@"tan" isEqualToString:operation]) {
+        NSLog(@"tan : performingOperation!");
+        self.operand = tan(self.operand);
+    }
+    else if ([@"1/x" isEqualToString:operation]) {
+        NSLog(@"1/x : performingOperation!");
+        
+        double inversion = 1/self.operand;
+        if(inversion == INFINITY) {
+            NSLog(@"Infinity baby - do nothing");
+        }
+        else {
+            NSLog(@"%f : inverted!", inversion);
+            self.operand = inversion;
+        }
+    }
+    
+    else if ([@"STO" isEqualToString:operation]) {
+        NSLog(@"STO : performingOperation!");
+        self.memory = self.operand;
+    }
+    else if ([@"RCL" isEqualToString:operation]) {
+        NSLog(@"RCL : performingOperation!");
+        self.operand = self.memory;
+    }
+    else if ([@"M+" isEqualToString:operation]) {
+        NSLog(@"M+ : performingOperation!");
+        self.memory = self.memory + self.operand;
+    }
+    //Clear memory only
+    else if ([@"MC" isEqualToString:operation]) {
+        NSLog(@"MC : performingOperation!");
+        self.memory = 0;
+    }
+    else if ([@"C" isEqualToString:operation]) {
+        NSLog(@"C : performingOperation!");
+        self.operand = 0;
+        self.waitingOperand = 0;
+        //self.memory = 0;
+        self.waitingOperation = nil;
+    }
+    //Pi button
+    else if ([@"π" isEqualToString:operation]) {
+        NSLog(@"π : performingOperation!");
+        self.operand = M_PI;
+    }
+    else {
+        [self performWaitingOperation];
+        self.waitingOperation = operation;
+        self.waitingOperand = self.operand;
+    }
+    return self.operand;
+}
+
+- (void) performWaitingOperation
+{
+    
+    if([@"+" isEqualToString:self.waitingOperation]) {
+        NSLog(@"+ : performingWaitingOperation!");
+        self.operand = self.waitingOperand + self.operand;
+    }
+    else if ([@"-" isEqualToString:self.waitingOperation]) {
+        NSLog(@"- : performingWaitingOperation!");
+        self.operand = self.waitingOperand - self.operand;
+    }
+    else if ([@"*" isEqualToString:self.waitingOperation]) {
+        NSLog(@"* : performingWaitingOperation!");
+        self.operand = self.waitingOperand * self.operand;
+    }
+    else if ([@"/" isEqualToString:self.waitingOperation]) {
+        NSLog(@"/ : performingWaitingOperation!");
+            if(self.operand)self.operand = self.waitingOperand / self.operand;
+    }
+}
+@end
