f7a5b3fa02420d1f9b4ac7c633023c3734b54375
Ruaidhri Hallinan <RuaidhriHallinan@gmail.com>
2013-02-04 21:59:59 +0000

    Renaming folders

---
+++ CalculatorAssignments/Assignment 3.2 ProgCalc/Calc/CalcModel.h
@@ -0,0 +1,26 @@
+//
+//  CalcModel.h
+//  Calc
+//
+//  Created by Ruaidhri Hallinan on 30/01/2013.
+//  Copyright (c) 2013 Ruaidhri Hallinan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalcModel : NSObject
+@property (nonatomic) double operand;
+@property (nonatomic) double waitingOperand;
+@property (nonatomic, strong) NSString *waitingOperation;
+@property (readonly,strong) id expression;
+@property (nonatomic) double memory;
+- (double) performOperation:(NSString *)operation;
+- (void)setVariableAsOperand:(NSString *)variableName;
++ (double)evaluateExpression:(id)anExpression
+         usingVariableValues:(NSDictionary *)variables;
++ (NSSet *)variablesInExpression:(id)anExpression;
+- (NSString *)descriptionOfExpression:(id)anExpression;
++ (id)propertyListForExpression:(id)anExpression;
+- (id)expressionForPropertyList:(id)propertyList;
+
+@end
